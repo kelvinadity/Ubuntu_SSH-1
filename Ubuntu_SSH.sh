928bdbaaaafe630e08ab48ac8db981b7f929c6de
kelvinzmodz <154925326+kelvinadity@users.noreply.github.com>
2024-08-15 01:54:47 +0700

    Update Ubuntu_SSH.sh

--- Ubuntu_SSH.sh
+++ Ubuntu_SSH.sh
@@ -30,7 +30,7 @@ install_ngrok_platform() {
       echo "----------------------------------------------------------------";
       echo "-- Mengunduh & Menginstal Platform Ngrok ...";
       echo "----------------------------------------------------------------";
-      curl -fsSL https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok.zip;
+      curl -fsSL https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-v3-stable-linux-amd64.zip -o ngrok.zip;
       unzip -q ngrok.zip ngrok;
       rm ngrok.zip;
       chmod +x ngrok;
